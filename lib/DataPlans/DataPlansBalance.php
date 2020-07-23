9b74e24c16b644fd1ecf89c574fbcb097dfda7d5
THEBUSTED <tksumeth@gmail.com>
2020-07-23 23:09:06 +0700

    Update class structure

--- lib/DataPlans/DataPlansBalance.php
+++ lib/DataPlans/DataPlansBalance.php
@@ -7,23 +7,22 @@ class DataPlansBalance extends DataPlansApiResource
      * Retrieves an account balance.
      *
      * @param  string $token
-     * @param  boolean $sandbox
      *
      * @return DataPlansBalance
      */
-    public static function retrieve($token = null)
+    public static function retrieve()
     {
-        return parent::g_retrieve(get_class(), self::getUrl(), $token);
+        return parent::doRetrieve(get_class(), self::getUrl());
     }
 
     /**
-     * (non-PHPdoc)
+     * Reload an account balance request
      *
-     * @see OmiseApiResource::g_reload()
+     * @see DataPlansApiResource::doReload()
      */
     public function reload()
     {
-        parent::g_reload(self::getUrl());
+        parent::doReload(self::getUrl());
     }
 
     /**
@@ -31,7 +30,7 @@ class DataPlansBalance extends DataPlansApiResource
      *
      * @return string
      */
-    private static function getUrl()
+    public static function getUrl()
     {
         return parent::getApiUrl(self::ENDPOINT);
     }
